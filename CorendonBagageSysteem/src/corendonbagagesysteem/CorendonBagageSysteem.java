42150e840e652e52d5201b792c3787f80d887099
Atori <Atori@145.28.154.190>
2016-11-15 11:31:07 +0100

    pleb

--- CorendonBagageSysteem/src/corendonbagagesysteem/CorendonBagageSysteem.java
+++ CorendonBagageSysteem/src/corendonbagagesysteem/CorendonBagageSysteem.java
@@ -22,7 +22,7 @@ public class CorendonBagageSysteem extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'pleb'");
+        btn.setText("Say 'plebian'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
