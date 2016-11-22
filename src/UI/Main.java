293d43c52312c63b6ffbe77648f5d4eea2f405f7
Elwin Slokker <Elwin Slokker@145.28.205.102>
2016-11-22 11:10:43 +0100

    added my main UI

---
+++ src/UI/Main.java
@@ -0,0 +1,40 @@
+package UI;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.Parent;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Elwin Slokker
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+        Parent page = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(page);
+        primaryStage.setTitle("Corendon Bagage Systeem");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        }catch (Exception ex){
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/UI/MainUIController.java
@@ -0,0 +1,43 @@
+package UI;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+/**
+ *
+ * @author Elwin Slokker
+ */
+public class MainUIController implements Initializable{
+    @FXML
+    private AnchorPane paneHeader;
+    private Button thaTest;
+    //private ImageView btnLangTurkish;
+    @FXML
+    private void testThis(ActionEvent event) {
+        //System.out.println("Turkish");
+        //paneHeader.getChildren().setAll(FXMLLoader.load(getClass().getResource("LanguageBox.fxml")));
+        try {
+            paneHeader.getChildren().add(FXMLLoader.load(getClass().getResource("LanguageBox.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(MainUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+
+
+    }   
+}
