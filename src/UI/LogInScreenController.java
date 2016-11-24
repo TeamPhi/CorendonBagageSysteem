4b76fffef164f32e675f5d5379fc67eff448682c
ElwinBran <elwinbran@gmail.com>
2016-11-24 09:05:12 +0100

    Inlogscherm als beginscherm
    Ok de inlogbutton werkt nu.
    De error zat in @FXML.
    We moeten wel kijken naar hoe we de root kunnen aanpassen of een omweg
    maken.

--- src/UI/LogInScreenController.java
+++ src/UI/LogInScreenController.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -19,12 +20,18 @@ public class LogInScreenController implements Initializable {
      * Initializes the controller class.
      */
     @FXML
+    AnchorPane anchorr;
+    @FXML
     private TextField textFieldGebruikersnaam;
+    @FXML
     private PasswordField passwordFieldWachtwoord;
+    @FXML
     private Button buttonInloggen;
     
+    @FXML
     private void logInAction(ActionEvent event) {
-        System.out.println("todo");
+        //anchorr.getChildren().
+
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
