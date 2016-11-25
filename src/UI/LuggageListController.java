6b6083e7a815194b7289dd8afd13797ad8365213
Oscar <Oscar@145.28.220.45>
2016-11-25 11:05:57 +0100

    test

---
+++ src/UI/LuggageListController.java
@@ -0,0 +1,19 @@
+package ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+/**
+ * FXML 
+ * 
+ * @author Elwin Slokker
+ */
+public class LuggageListController implements Initializable{
+     /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
