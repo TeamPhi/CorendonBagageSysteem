92ad785168bf3c30090207048cdaaa8a5b76f90b
jrquak <jr.quak@ziggo.nl>
2016-11-29 11:44:16 +0100

    Connectie versie2

--- src/backend/DBConnection.java
+++ src/backend/DBConnection.java
@@ -1,22 +1,25 @@
 package backend;
 
 /**
- *
+ * This class establishes a connection to a specified database.
  * @author Jordy Quak
  */
 import java.sql.*;
 
 public class DBConnection {
 
-    Connection conn = null;
+    private static Connection conn;  
+    private static String url = "dbc:mysql://localhost:3306/<database>";//Location of database
+    private static String user = "root";//Username of database  
+    private static String pass = "password";//Password of database 
 
-    public static Connection ConnecrDb() {
+    public static Connection ConnectDb() {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            Connection conn = DriverManager.getConnection("dbc:mysql://localhost:3306/<database>", "root", "PASSWORD");
-            System.out.println("Connection success!");
+            Connection conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connection success");
             return conn;
 
         } catch (Exception e) {
