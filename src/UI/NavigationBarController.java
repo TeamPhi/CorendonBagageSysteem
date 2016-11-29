fb7680a81c256b0b3aed113b5d5830199dad042d
mark6133 <mark18steneker@gmail.com>
2016-11-29 15:55:28 +0100

    2e poging
    Blijkbaar was dit niet gecommit

--- src/UI/NavigationBarController.java
+++ src/UI/NavigationBarController.java
@@ -1,8 +1,11 @@
-package UI;
+package ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -11,6 +14,19 @@ import javafx.fxml.Initializable;
  */
 public class NavigationBarController implements Initializable {
 
+    @FXML
+    private Button buttonHome;
+    @FXML
+    private Button buttonLuggageOverview;
+    @FXML
+    private Button buttonClaims;
+    @FXML
+    private Button buttonStatistics;
+    @FXML
+    private Button buttonAccountManager;
+    @FXML
+    private Button buttonSignOut;
+
     /**
      * Initializes the controller class.
      */
@@ -18,5 +34,29 @@ public class NavigationBarController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void homeButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void luggageOverviewButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void claimsButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void statisticsButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void accountmanagementButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void logOutButtonClicked(ActionEvent event) {
+    }
     
 }
