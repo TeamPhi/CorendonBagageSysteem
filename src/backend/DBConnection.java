c14f6703afe8a991f26cfc08a083fc24f98a6881
Oscar Hulshof <o.q.hulshof@quicknet.nl>
2016-12-02 13:11:17 +0100

    eerste query in java
    query toegevoegd maar nog niet getest of het werkt

--- src/backend/DBConnection.java
+++ src/backend/DBConnection.java
@@ -2,18 +2,20 @@ package backend;
 
 /**
  * This class establishes a connection to a specified database.
+ *
  * @author Jordy Quak
  */
 import java.sql.*;
 
 public class DBConnection {
 
-    private static Connection conn;  
+    private static Connection conn;
     private static String url = "dbc:mysql://localhost:3306/<database>";//Location of database
     private static String user = "root";//Username of database  
     private static String pass = "password";//Password of database 
+
     /**
-     * 
+     *
      * @return A connection object which can be used to reach the database.
      */
     public static Connection connectDb() {
@@ -32,4 +34,21 @@ public class DBConnection {
         }
     }
 
+    public static void addFoundLuggage(String dbName, Date date,
+            Time time, String airport) throws SQLException {
+        Statement stmt = null;
+        //query moet nog ingevoerd worden
+        String addLuggage = ("INSERT INTO foundLuggage (date, time, airport) VALUES (" +
+                date + ", \"" + time + "\", " + airport + ");");
+        try {
+            stmt = DBConnection.conn.createStatement();
+            stmt.executeQuery(addLuggage);
+        } catch (SQLException e) {
+
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
 }
