b85cf231e9c08654ee17fbc6faee4146483cee1f
ikarugnas <ikarugnas@gmail.com>
2016-12-13 17:11:11 +0100

    part 2

--- src/UI/AccountManagerController.java
+++ src/UI/AccountManagerController.java
@@ -1,7 +1,14 @@
 package ui;
 
+import backend.Account;
+import backend.FoundLuggage;
+import backend.LuggageEnum;
+import backend.StatusEnum;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,6 +18,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import static ui.LuggageListController.foundLuggageData;
 
 /**
  * FXML Controller class
@@ -19,27 +28,32 @@ import javafx.scene.control.TextField;
  */
 public class AccountManagerController implements Initializable {
 
+    public static ObservableList<Account> accountData;
+    
     @FXML
     private Button buttonAccountManagerEdit;
     @FXML
     private Button buttonAccountManagerAdd;
     @FXML
-    private TableView<?> tableAccount;
+    private TableView<Account> tableAccount;
     @FXML
-    private TableColumn<?, ?> columnUsername;
+    private TableColumn<Account, String> columnUsername;
     @FXML
-    private TableColumn<?, ?> columnPassword;
+    private TableColumn<Account, String> columnPassword;
     @FXML
-    private TableColumn<?, ?> columnPriveleges;
+    private TableColumn<Account, String> columnPriveleges;
     @FXML
-    private TableColumn<?, ?> columnName;
+    private TableColumn<Account, String> columnName;
     @FXML
-    private TableColumn<?, ?> columnSurname;
+    private TableColumn<Account, String> columnSurname;
     @FXML
-    private TableColumn<?, ?> columnEmail;
+    private TableColumn<Account, String> columnEmail;
     
     public AccountManagerController(){
-        
+        //The list is filled with test data.
+        AccountManagerController.accountData = FXCollections.observableArrayList(
+                new Account("Karello", "sterstersterster", 
+                "Baliemedewerker", "Karel", "Lout", "Karello@gmail.com"));
     }
     
     /**
@@ -56,6 +70,23 @@ public class AccountManagerController implements Initializable {
         I18N.bindTableText(this.columnName.getText(), this.columnName, (Object[]) null);
         I18N.bindTableText(this.columnSurname.getText(), this.columnSurname, (Object[]) null);
         I18N.bindTableText(this.columnEmail.getText(), this.columnEmail, (Object[]) null);
+        this.tableAccount.setItems(accountData);
+        
+        //init collumns and decide what values are stored in them.
+        this.columnUsername.setCellValueFactory(
+                new PropertyValueFactory<Account, String>("username"));
+        this.columnPassword.setCellValueFactory(
+                new PropertyValueFactory<Account, String>("password"));
+        this.columnPriveleges.setCellValueFactory(
+                new PropertyValueFactory<Account, String>("privileges"));
+        this.columnName.setCellValueFactory(
+                new PropertyValueFactory<Account, String>("name"));
+        this.columnSurname.setCellValueFactory(
+                new PropertyValueFactory<Account, String>("surname"));
+        this.columnEmail.setCellValueFactory(
+                new PropertyValueFactory<Account, String>("email"));
+        
+        this.tableAccount.setItems(accountData);
     }    
 
     @FXML
