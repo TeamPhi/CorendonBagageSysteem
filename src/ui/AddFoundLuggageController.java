b1ae5fcc123768c17ebcfd413ffaa3bfa66b3c24
jrquak <jr.quak@ziggo.nl>
2016-12-30 16:18:42 +0100

    Poging tot addluggage werkend te krijgen
    maar keihard gefaald en ik kom er nu achter dat er emojis hier kunnen
    :neckbeard:

--- src/ui/AddFoundLuggageController.java
+++ src/ui/AddFoundLuggageController.java
@@ -1,18 +1,32 @@
 package ui;
 
+import backend.Account;
+import backend.DBConnection;
 import backend.FoundLuggage;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Elwin Slokker
+ * @author Jordy Quak
+ */
 
 public class AddFoundLuggageController implements Initializable {
 
@@ -136,4 +150,114 @@ public class AddFoundLuggageController implements Initializable {
         //System.out.println("delete"); COMPLETE 
 
     }
-}
+    
+//    @FXML
+//    private void save(ActionEvent event) {
+//        //if all fields are filled in correctly.
+//        if (this.textFieldTime.getText().isEmpty() == false
+//                || this.textFieldLostFound.getText().isEmpty() == false
+//                || this.textFieldAirport.getText().isEmpty() == false
+//                || this.textFieldBrand.getText().isEmpty() == false
+//                || this.textFieldColor.getText().isEmpty() == false
+//                || this.textFieldDate.getText().isEmpty() == false) {
+//                //create account object from textfields.
+//                FoundLuggage temp = new FoundLuggage(
+//                        this.textFieldLabelId.getText(),
+//                        this.textFieldFlightId.getText(),
+//                        this.textFieldAirport.getText(),
+//                        this.textFieldLostFound.getText(),
+//                        this.textFieldDestination.getText(),
+//                        this.textFieldType.getText(),
+//                        this.textFieldBrand.getText(),
+//                        this.textFieldColor.getText(),
+//                        this.textFieldDate.getText(),
+//                        this.textFieldStatus.getText());
+//                //create DB connection.
+//                Connection conn = DBConnection.connectDb();
+//                try {
+//                    ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM account WHERE username = \'"
+//                            + temp.getUsername() + "\';");
+//                    /*
+//                First check if the user made an original username.
+//                     */
+//                    int counter = 0;
+//                    while (rs.next()) {
+//                        counter++;
+//                    }
+//                    if (counter < 1) {
+//                        /*
+//                    Depending whether the user is editing an account or adding one,
+//                    add or replace the temp account in the database.
+//                         */
+//                        if (this.addMode) {
+//
+//                            String sql = "INSERT INTO lostluggage (userid, username, password,"
+//                                    + " privilege, name, surname, email)"
+//                                    + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+//                            PreparedStatement pst = conn.prepareStatement(sql);
+//                            
+//                            pst.setString(1, this.textFieldLabelId.getText());
+//                            pst.setString(2, this.textFieldFlightId.getText());
+//                            pst.setString(3, this.textFieldAirport.getText());
+//                            pst.setString(4, this.textFieldLostFound.getText());
+//                            pst.setString(5, this.textFieldDestination.getText());
+//                            pst.setString(6, this.textFieldType.getText());
+//                            pst.setString(7, this.textFieldBrand.getText());
+//                            pst.setString(8, this.textFieldColor.getText());
+//                            pst.setString(9, this.textFieldDate.getText());
+//                            pst.setString(10, this.textFieldStatus.getText());
+//                            pst.executeUpdate();
+//
+//                            //if the data is put in the database, add it to the tableview.
+//                            //AccountManagerController.accountData.add(temp);
+//
+//                        } else {
+//
+//                            String sql = "UPDATE account SET username = ?, password = ?,"
+//                                    + " privilege = ?, name = ?, surname = ?, email = ? "
+//                                    + "WHERE userid = ?";
+//                            PreparedStatement pst = conn.prepareStatement(sql);
+//                            pst.setString(1, this.textFieldLabelId.getText());
+//                            pst.setString(2, this.textFieldFlightId.getText());
+//                            pst.setString(3, this.textFieldAirport.getText());
+//                            pst.setString(4, this.textFieldLostFound.getText());
+//                            pst.setString(5, this.textFieldDestination.getText());
+//                            pst.setString(6, this.textFieldType.getText());
+//                            pst.setString(7, this.textFieldBrand.getText());
+//                            pst.setString(8, this.textFieldColor.getText());
+//                            pst.setString(9, this.textFieldDate.getText());
+//                            pst.setString(10, this.textFieldStatus.getText());
+//                            //preparedStatement.setInt(7, temp.getUserID);
+//                            pst.executeUpdate();
+//                            
+//                            //replace the account in the table as well.
+//                            //int replaceIndex = AccountManagerController.accountData.indexOf(this.editable);
+//                            //AccountManagerController.accountData.remove(replaceIndex);
+//                            //AccountManagerController.accountData.add(replaceIndex, temp);
+//                            //AccountManagerController.accountData.add(temp);
+//
+//                        }
+//                        //finally, close the window
+//                        Stage stage = (Stage) this.buttonSave.getScene().getWindow();
+//                        stage.close();
+//                    } else {
+//                        //error message
+//                        Alert alert = new Alert(Alert.AlertType.WARNING);
+//                        alert.setTitle("Registration failure");//ripe for translation
+//                        alert.setHeaderText(null);
+//                        alert.setContentText("The username already exists.");//ripe for translation
+//                        alert.showAndWait();
+//                    }
+//                    conn.close();
+//                } catch (SQLException ex) {
+//                    System.err.println("Error" + ex);
+//                }
+//            } else {
+//            //error message
+//            Alert alert = new Alert(Alert.AlertType.WARNING);
+//            alert.setTitle("Registration failure");//ripe for translation
+//            alert.setHeaderText(null);
+//            alert.setContentText("One or more of the fields is empty.");//ripe for translation
+//            alert.showAndWait();
+//        }
+    }
